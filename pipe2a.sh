e0c36b7f0b38d6ad872529d06fe9f802ef69813c
AWS ParallelCluster user <ec2-user@ip-10-0-14-207.ap-southeast-1.compute.internal>
2026-09-06 09:56:47 +0000

    upload all files for lab 5

---
+++ pipe2a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# First job: independent
+jid1=$(sbatch /data/src/PyHipp/myrplparallel-slurm.sh)
+
+# second job - no dependencies, called from the day directory
+jid2=$(sbatch /data/src/PyHipp/rse-slurm.sh)
+
+# third set of jobs - depends on rse-slurm.sh, called from the day directory
+jid3=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs1a-slurm.sh)
+jid4=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs2a-slurm.sh)
+jid5=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs3a-slurm.sh)
+jid6=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs4a-slurm.sh)
+
